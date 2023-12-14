08c2a37b84a5eb32f3c95d53c1af59f2c59b488a
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2023-12-14 17:03:10 +0100

    Implementation de la classe Carte.java

--- README.md
+++ README.md
@@ -2,11 +2,27 @@
 <p>Le bruit de perlin est une fonction mathématique qui permet de générer des images aléatoires. Il est utilisé dans les jeux vidéos pour générer des terrains aléatoires. Il est aussi utilisé dans les films pour générer des textures.</p>
 
 <h2> Question du sujet </h2>
-<h4>Q1.</h4>
+<h4>Q1.pourquoi l'attribut graine est-il en final ?</h4>
 <p>L'attribut graine est en final car une fois que la graine à été défini il ne faut pas qu'elle puisse être modifier</p>
 
-<h4>Q2.</h4>
+<h4>Q2. Réaliser l'architecture que prendrait la forme d'une telle modélisation sous forme d'un diagramme UML. Quelle serait la différence d'attributs et de méthodes entre ces classes ? Expliquer, en s'appuyant sur un exercice vu en cours, pourquoi c'est une mauvaise idée. Quelle est la solution qu'il faudrait préférer (et que l'on va préférer) ?</h4>
 <img src="data/ReadMe/UML.png">
 Il n'y aurait aucune différence d'attributs
 <p>C'est une très mauvaise idée d'utiliser cette modélisation,en effet elle ne respecte pas les principe ouvert/fermer 
 et rend la maintenance difficile. Le mieux serait d'utiliser un pattern visiteur .</p>
+
+<h4>Q3. quel type utilisez-vous ?</h4>
+<p>
+Les différents type des variables sont :
+</p>
+    <ul>
+        <li>Hydrométrie :  Double </li>
+        <li>Température : Double </li>
+        <li>Altitude : Double </li>
+    </ul> 
+<p>J'ai choisi de les mettre en double car il faut pouvoir avoir des valeurs décimales double précision. </p>
+
+
+<h4>Q4. Pourquoi sortir, selon vous, ce bout d'algorithme de la classe ?</h4>
+<p>Selon moi il faut sortir ce bout d'algorythme car pour chaque ajout de nouveau type de terrain il faudrait modifier le code et donc potentiellement introduire une erreur dans un code qui de base fonctionne bien </p>
+
---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/carte/Carte.java
@@ -0,0 +1,77 @@
+package fr.ecole3il.rodez2023.perlin.terrain.carte;
+
+import fr.ecole3il.rodez2023.perlin.terrain.carte.TerrainInexistant;
+import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
+import fr.ecole3il.rodez2023.perlin.terrain.generation.GenerateurCarte;
+
+import java.util.Scanner;
+
+public class Carte {
+
+    private String nom;
+    private int largeur;
+    private int hauteur;
+    private Terrain[][] terrains;
+
+
+    /**
+     * Constructeur de la classe Carte.
+     * @param nom Le nom de la carte.
+     * @param largeur La largeur de la carte.
+     * @param hauteur La hauteur de la carte.
+     * @param generateurCarte Le générateur de carte utilisé pour générer la carte.
+     */
+    public Carte(String nom, int largeur, int hauteur, GenerateurCarte generateurCarte) {
+        this.nom = nom;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.terrains = generateurCarte.genererCarte(largeur, hauteur);
+    }
+
+    public Carte (String donneesCarte){
+        Scanner scanner = new Scanner(donneesCarte);
+        this.nom = scanner.nextLine();
+        this.largeur = scanner.nextInt();
+        this.hauteur = scanner.nextInt();
+        this.terrains = new Terrain[largeur][hauteur];
+        scanner.close();
+        //TODO : remplir le tableau de terrains (alt, hyd, temp)
+    }
+
+    /**
+     * Récupère le nom de la carte.
+     * @return  Le nom de la carte.
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Récupère la largeur de la carte.
+     * @return La largeur de la carte.
+     */
+    public int getLargeur() {
+        return largeur;
+    }
+
+    /**
+     * Récupère la hauteur de la carte.
+     * @return La hauteur de la carte.
+     */
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    /**
+     * Récupère le terrain aux coordonnées spécifiées.
+     * @param x La coordonnée x du terrain.
+     * @param y La coordonnée y du terrain.
+     * @return Le terrain aux coordonnées spécifiées.
+     */
+    public Terrain getTerrain(int x, int y) {
+        if (x < 0 || x >= largeur || y < 0 || y >= hauteur) {
+            throw new TerrainInexistant();
+        }
+        return terrains[x][y];
+    }
+}
